1372ea82df09a43c692760320076c70a17f1640c
cbcote <christopher.cote88@gmail.com>
2024-05-26 10:39:40 -0500

    chore: Update plot_results method with type hints and docstring

--- simulations/results.py
+++ simulations/results.py
@@ -2,7 +2,13 @@ import matplotlib.pyplot as plt
 
 class SimulationResults:
     @staticmethod
-    def plot_results(results):
+    def plot_results(results: list) -> None:
+        """
+        Plot the results of a betting simulation
+        
+        Args:
+            results (list): A list of player balances after each round
+        """
         plt.plot(results)
         plt.xlabel('Rounds')
         plt.ylabel('Balance')
