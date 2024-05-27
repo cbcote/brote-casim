8b527b718f39fbc73476935c1cb64ea36c722724
cbcote <christopher.cote88@gmail.com>
2024-05-27 10:12:22 -0500

    chore: Refactor Deck class to support multiple decks

--- deck.py
+++ deck.py
@@ -5,10 +5,15 @@ class Deck:
     suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
     ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
 
-    def __init__(self):
+    def __init__(self, num_decks=6):
         """Initialize a deck object with 52 cards in it and shuffle it"""
+        self.num_decks = num_decks
         self.cards = [Card(suit, rank) for suit in self.suits for rank in self.ranks]
         self.shuffle()
+    
+    def _create_deck(self):
+        """Create a deck of cards"""
+        return [Card(suit, rank) for suit in self.suits for rank in self.ranks] * self.num_decks
 
     def shuffle(self):
         """Shuffle the deck of cards"""
@@ -16,4 +21,11 @@ class Deck:
 
     def deal_card(self):
         """Deal a card from the deck"""
+        if len(self.cards) == 0:
+            self.cards = self._create_deck()
+            self.shuffle()
         return self.cards.pop()
+    
+    def cards_remaining(self):
+        """Return the number of cards remaining in the deck"""
+        return len(self.cards)
